0b1b687f1c26809f02af32b6d84d7d8a635f327b
ariaaaawww <ariantowp0@gmail.com>
2026-08-04 13:10:34 +0700

    first commit

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $title = "Sistem Sekolah - Daftar Siswa";
+        $description = "Menampilkan daftar siswa yang terdaftar di sekolah";
+        $students = [
+            [
+                'id' => 1,
+                'nis' => '1001',
+                'name' => 'Andi',
+                'class' => 'XII TKJ 1',
+                'major' => 'TKJ',
+            ],
+            [
+                'id' => 2,
+                'nis' => '1002',
+                'name' => 'Budi',
+                'class' => 'XII AKL 1',
+                'major' => 'AKL',
+            ],
+        ];
+
+        return view('students.index', 
+        [
+            'title' => $title,
+            'description' => $description,
+            'students' => $students,
+        ]);
+    }
+
+    public function create()
+    {
+        $title = "Sistem Sekolah - Tambah Siswa";
+        $description = "Menambahkan data siswa baru";
+
+        return view('students.create', compact('title', 'description'));
+    }
+
+    public function store()
+    {
+        return "Melakukan penambahan data siswa";
+    }
+
+    public function show(string $id)
+    {
+        $title = "Sistem Sekolah - Detail Siswa";
+        $description = "Menampilkan detail data siswa";
+        return view('students.show', [
+            'title' => $title,
+            'description' => $description,
+        ]);
+    }
+
+    public function edit(string $id)
+    {
+        $title = "Sistem Sekolah - Edit Siswa";
+        $description = "Memperbarui data siswa";
+
+        return view('students.edit', compact('title', 'description'));
+    }
+
+    public function update(string $id)
+    {
+        return "Melakukan perubahan data siswa";
+    }
+
+    public function destroy(string $id)
+    {
+        return "Menghapus data siswa";
+    }
+
+}
---
+++ app/Http/Controllers/TeachersController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TeacherController extends Controller
+{
+    public function index()
+    {
+        return "Menampilkan halaman daftar guru";
+    }
+
+    public function create()
+    {
+        return "Menampilkan halaman tambah guru";
+    }
+
+    public function store()
+    {
+        return "Melakukan penambahan data guru";
+    }
+
+    public function show(string $id)
+    {
+        return "Menampilkan guru dengan ID: {$id}";
+    }
+
+    public function edit(string $id)
+    {
+        return "Menampilkan halaman edit guru";
+    }
+
+    public function update(string $id)
+    {
+        return "Melakukan perubahan data guru";
+    }
+
+    public function destroy(string $id)
+    {
+        return "Menghapus data guru";
+    }
+}
